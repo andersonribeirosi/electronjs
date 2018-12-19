f0088f290ef2c9242d3462599420f85bd78bb6c7
wesley <wesleygonalv@gmail.com>
2018-12-19 19:39:18 -0200

    Listagem dos clientes cadastrados no sistema

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 1300, height: 700, frame: false, transparent: false})
 
   // and load the index.html of the app.
-  mainWindow.loadFile('cadastroclientes.html')
+  mainWindow.loadFile('index.html')
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
